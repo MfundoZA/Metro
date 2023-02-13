c1f39ead796f6c942d9ac6954c493af9d33633e0
MfundoZA <jordantenza@gmail.com>
2023-02-13 11:33:56 +0200

    Set up the structure of the script

--- Metro/Program.cs
+++ Metro/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿var input = args[0];
+
+while (input != "stop")
+{
+
+    switch (input.ToLower())
+    {
+        case "hi":
+            // log date and time
+            // save locally
+
+            Console.WriteLine("Hi");
+            break;
+
+        case "bye":
+            // log last task duration
+            // log clock off time
+            // save locally
+
+            Console.WriteLine("Bye");
+            break;
+
+        case "rpt":
+            // display productivity for the last 30 days
+            break;
+
+        case "rpt-year":
+            // display productivity for the last 12 months
+            break;
+
+        case "rpt-day":
+            // display productivity for the last 24 hours
+            break;
+
+        case "help":
+            // call method that will display all commands
+            break;
+
+        default:
+            // call method that will dispaly all commands
+            break;
+
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.ReadKey();
