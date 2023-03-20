4133242925476ae22151d04048c178ee44d8a199
MfundoZA <jordantenza@gmail.com>
2023-03-20 09:22:53 +0200

    Created Task class

---
+++ Metro/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro.Models
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public List<Tuple<DateTime, DateTime>> Sessions { get; set; }
+        public bool IsComplete { get; set; }
+
+        public Task() { }
+
+        public Task(string description, List<Tuple<DateTime, DateTime>> sessions, bool isComplete)
+        {
+            Description = description;
+            Sessions = sessions;
+            IsComplete = isComplete;
+        }
+    }
+}
