148bff755f61cc68774a241e682c9513478bc806
MfundoZA <jordantenza@gmail.com>
2023-03-20 08:50:24 +0200

    Create WorkDay class

---
+++ Metro/Models/WorkDay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro.Models
+{
+    public class WorkDay
+    {
+        public DateTime ClockInTime { get; set; }
+        public DateTime ClockOutTime { get; set;}
+
+        public WorkDay()
+        {
+
+        }
+
+        public WorkDay(DateTime clockInTime, DateTime clockOutTime)
+        {
+            ClockInTime = clockInTime;
+            ClockOutTime = clockOutTime;
+        }
+    }
+}
