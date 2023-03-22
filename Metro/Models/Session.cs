39fba8df93d44cde9a924df9d42fdf9d5666ce8d
MfundoZA <jordantenza@gmail.com>
2023-03-22 06:57:45 +0200

    Created Session class

---
+++ Metro/Models/Session.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Metro.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public Session() { }
+
+        public Session(DateTime startTime, DateTime endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public override string ToString() => JsonSerializer.Serialize<Session>(this);
+    }
+}
