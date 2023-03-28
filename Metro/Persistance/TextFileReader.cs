a62026e23452ace86960bbe529897cde6db42a4b
MfundoZA <jordantenza@gmail.com>
2023-03-28 17:40:51 +0200

    Implemented TextFileReader class

---
+++ Metro/Persistance/TextFileReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro.Persistance
+{
+    public class TextFileReader
+    {
+        public StreamReader? StreamReader { get; set; }
+
+        public static string ReadAll(string path)
+        {
+            try
+            {
+                return File.ReadAllText(path);
+            }
+            catch (Exception ex) when (ex is FileNotFoundException)
+            {
+                throw new FileNotFoundException(ex.Message);
+            }
+        }
+    }
+}
