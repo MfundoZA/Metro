b70bcb35fd204e8462c23782d45cc3d3a71def65
MfundoZA <jordantenza@gmail.com>
2023-04-16 12:20:00 +0200

    Created ClockInCommand

---
+++ Metro/Commands/ClockInCommand.cs
@@ -0,0 +1,29 @@
+﻿using Metro.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro.Commands
+{
+    public class ClockInCommand : Command
+    {
+        public DateTime TimeClockedIn { get; set; }
+
+        public ClockInCommand()
+        {
+            TimeClockedIn = DateTime.Now;
+        }
+
+        public ClockInCommand(DateTime timeClockedIn)
+        {
+            TimeClockedIn= timeClockedIn;
+        }
+
+        public void Execute()
+        {
+            TextFileWriter.Write(TimeClockedIn, "Workdays.json");
+        }
+    }
+}
