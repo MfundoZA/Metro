79ada45802806879f9588c6878f6f8c58e41720a
MfundoZA <jordantenza@gmail.com>
2023-05-06 20:51:55 +0200

    Created a copy of the write method that accepts strings as a parameter

--- Metro/Persistance/TextFileWriter.cs
+++ Metro/Persistance/TextFileWriter.cs
@@ -12,6 +12,21 @@ namespace Metro.Persistance
     {
         private static StreamWriter? StreamWriter;
 
+        public static void Write(string text, string path)
+        {
+            try
+            {
+                StreamWriter = File.AppendText(path);
+                StreamWriter.WriteLine(text);
+                StreamWriter.Close();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("An error occured! Please try again. \n" +
+                    $"Error message: {exception.Message}");
+            }
+        }
+
         public static void Write(object data, string path)
         {
             try
