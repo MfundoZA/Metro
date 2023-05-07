f5b56c44ad7eadfb9cb492c3cc02acb0b4c43fb8
MfundoZA <jordantenza@gmail.com>
2023-05-07 12:11:54 +0200

    Fixed bugs in ClockInCommand Execution

--- Metro/Commands/ClockInCommand.cs
+++ Metro/Commands/ClockInCommand.cs
@@ -1,4 +1,5 @@
-﻿using Metro.Persistance;
+﻿using Metro.Models;
+using Metro.Persistance;
 using Spectre.Console.Cli;
 using System;
 using System.Collections.Generic;
@@ -15,12 +16,21 @@ namespace Metro.Commands
 {
     public class ClockInCommand : Command<ClockInSettings>
     {
+        private const string FILE_NAME = "Workdays.json";
+
         public override int Execute([NotNull] CommandContext context, [NotNull] ClockInSettings settings)
         {
-            var clockIn = DateTime.Now.ToString();
-            var jsonData = JsonSerializer.Serialize(clockIn);
+            var clockIn = DateTime.Now;
+            var workday = new WorkDay(clockIn);
 
-            TextFileWriter.Write(jsonData, "Workdays.json");
+            var workdays = TextFileReader.ReadAllAsList<WorkDay>(FILE_NAME);
+            workdays.Add(workday);
+
+            using (var fileStream = new FileStream(FILE_NAME, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize(fileStream, workdays);
+            }
+
             return 0;
         }
     }
