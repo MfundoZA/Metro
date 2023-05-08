c9a7ef9be147dc1271e8f0cab548bbce7c2a56c0
MfundoZA <jordantenza@gmail.com>
2023-05-08 11:50:48 +0200

    Implemented logic to allow user to update current day's clock in time

--- Metro/Commands/ClockInCommand.cs
+++ Metro/Commands/ClockInCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Metro.Models;
 using Metro.Persistance;
+using Spectre.Console;
 using Spectre.Console.Cli;
 using System;
 using System.Collections.Generic;
@@ -39,7 +40,26 @@ namespace Metro.Commands
             currentWorkday = new WorkDay(clockInTime);
 
             var workdays = TextFileReader.ReadAllAsList<WorkDay>(FILE_NAME);
-            workdays.Add(workday);
+
+            var previousWorkday = workdays?.Last();
+
+            if (workdays != null && previousWorkday?.ClockInTime.Date == currentWorkday.ClockInTime.Date)
+            {
+                if (settings.Force == true || AnsiConsole.Confirm("Warning! You have already clocked in today. Do you want to override?"))
+                {
+                    workdays.Remove(workdays.Last());
+                    workdays.Add(currentWorkday);
+                }
+            }
+            else
+            {
+                if (workdays == null)
+                {
+                    workdays = new List<WorkDay>();
+                }
+
+                workdays.Add(currentWorkday);
+            }
 
             using (var fileStream = new FileStream(FILE_NAME, FileMode.OpenOrCreate))
             {
